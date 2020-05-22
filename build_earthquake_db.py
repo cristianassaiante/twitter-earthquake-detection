e2e973ee6af7466a1576af53ecf4e0f9d5e6e5ac
SigTermGh <tarantino.1760424@studenti.uniroma1.it>
2020-05-22 17:39:57 +0200

    meh

--- build_earthquake_db.py
+++ build_earthquake_db.py
@@ -1,7 +1,7 @@
 import requests
 import json
 import sqlite3
-
+from datetime import date
 
 connection = sqlite3.connect("earthquakes.db")
 c = connection.cursor()
@@ -16,33 +16,42 @@ c.execute("""
 
 data = []
 
-for year in range(2000, 2019):
+for year in range(2019, 2021):
     
     # yyyy-mm-dd because the world is a bad place and we all deserve to die
-    url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=%d-01-01&endtime=%d-01-01&minmagnitude=5" % (year, year + 1)
+    url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=%d-01-01&endtime=%d-01-01&minmagnitude=3" % (year, year + 1)
 
     response = requests.get(url)
     
-    #for earthquake in response.json()['features']:
-        #print(earthquake['properties']['alert'])
-    #continue
-    
-    #print(json.dumps(response.json()['features'][0], indent=4, sort_keys=True)); exit()
-    
     for earthquake in response.json()['features']:
         if earthquake['properties']['type'] == 'earthquake': # there are a few nuclear explosions, what you gonna do
-            data.append( (
-                            earthquake['id'],                         # Unique id
-                            earthquake['geometry']['coordinates'][1], # South
-                            earthquake['geometry']['coordinates'][0], # East
-                            earthquake['properties']['time'],         # Timestamp
-                            earthquake['properties']['mag'],          # Magnitude
-                            earthquake['properties']['magType'],      # Magnitude type (?)
-                            earthquake['properties']['alert'],        # Alert (green yellow orange red)
-                            earthquake['properties']['felt']          # How many people reported it
-                         )
-                        )
-        
+            try:
+                date.fromtimestamp(earthquake['properties']['time'])
+            except:  # timestamp in milliseconds
+                data.append( (
+                                earthquake['id'],                         # Unique id
+                                earthquake['geometry']['coordinates'][1], # South
+                                earthquake['geometry']['coordinates'][0], # East
+                                earthquake['properties']['time'] // 1000, # Timestamp
+                                earthquake['properties']['mag'],          # Magnitude
+                                earthquake['properties']['magType'],      # Magnitude type (?)
+                                earthquake['properties']['alert'],        # Alert (green yellow orange red)
+                                earthquake['properties']['felt']          # How many people reported it
+                            )
+                            )
+            else:   # timestamp in seconds
+                data.append( (
+                                earthquake['id'],                         # Unique id
+                                earthquake['geometry']['coordinates'][1], # South
+                                earthquake['geometry']['coordinates'][0], # East
+                                earthquake['properties']['time'],         # Timestamp
+                                earthquake['properties']['mag'],          # Magnitude
+                                earthquake['properties']['magType'],      # Magnitude type (?)
+                                earthquake['properties']['alert'],        # Alert (green yellow orange red)
+                                earthquake['properties']['felt']          # How many people reported it
+                            )
+                            )
+            
     c.executemany('insert into earthquakes values (?, ?, ?, ? ,?, ?, ?, ?)', data)
     print("Added %d events" % len(data))
     data.clear()
